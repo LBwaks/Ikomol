1ba040597c69cd5b42ba73721d4ccabc9ff7b97d
Lucas Obwaku <obwakuvictor@gmail.com>
2022-09-22 13:55:08 +0300

    settings changes 4

--- Ikomol/settings.py
+++ Ikomol/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'Ikomol.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-}
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASES = {
+#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+# }
 
 
 DATABASES = {
