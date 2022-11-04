585bc7c6a5e28b9f8dfd705aedeb69fc863889dd
LBwaks <obwakuvictor@gmail.com>
2022-11-04 08:29:17 +0300

    Fix pyscog

--- Ikomol/settings.py
+++ Ikomol/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 # from decouple import config
 import os
 from dotenv import load_dotenv
-# import dj_database_url
+import dj_database_url
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,10 +152,10 @@ WSGI_APPLICATION = 'Ikomol.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
-# DATABASES = {
-#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1200),
-# }
+DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1200),
+}
 
 
 # DATABASES = {
