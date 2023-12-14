eb3cce28512a0b6247754d8f21bd1e6e3e37a550
LBwaks <obwakuvictor@gmail.com>
2023-12-14 21:14:43 +0300

    error

--- Ikomol/urls.py
+++ Ikomol/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import stat
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -41,5 +41,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler500 = "core.views.error_500"
-# handler404 = 'core.views.error_404'
+handler404 = 'core.views.error_404'
 admin.site.site_header = "iKomol Internet"
